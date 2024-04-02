b1d4aa215f6a77ca215cdd0adb73964c030c502c
ampgn <aboucal9@gmail.com>
2024-04-02 14:09:30 +0200

    New key

--- src/server/func/crypto.ts
+++ src/server/func/crypto.ts
@@ -2,15 +2,7 @@ import * as Crypto from 'crypto'
 import { readFileSync } from 'fs';
 
 const algo = "SHA256";
-let privateKey;
-
-if (process.env.PRIVATE_KEY) {
-    // Utiliser la clé privée à partir de la variable d'environnement sur Heroku.
-    privateKey = JSON.parse(process.env.PRIVATE_KEY);
-} else {
-    // Charger la clé privée à partir du fichier privatekey.pem en développement.
-    privateKey = readFileSync('privatekey.pem');
-}
+const privateKey = readFileSync('privatekey.pem')
 
 export function sign (str: string): string {
     return Crypto.sign(algo, Buffer.from(str), privateKey).toString("base64");
